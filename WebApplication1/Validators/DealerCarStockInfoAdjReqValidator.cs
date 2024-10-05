ea3144f07e3abbf476ea1b185dfa077ef062589f
Rex Wong <wongkingyu7@gmail.com>
2024-10-05 17:32:15 +1000

    Update the Validation of car info update request stock id now is not empty

--- WebApplication1/Validators/DealerCarStockInfoAdjReqValidator.cs
+++ WebApplication1/Validators/DealerCarStockInfoAdjReqValidator.cs
@@ -7,7 +7,7 @@ public class DealerCarStockInfoAdjReqValidator : AbstractValidator<DealerCarStoc
 {
     public DealerCarStockInfoAdjReqValidator()
     {
-        RuleFor(req => req.stockid).NotNull().GreaterThanOrEqualTo(0);
+        RuleFor(req => req.stockid).NotEmpty();
         RuleFor(req => req.make).NotEmpty().MaximumLength(50);
         RuleFor(req => req.model).NotEmpty().MaximumLength(50);
         RuleFor(req => req.year).NotNull().GreaterThanOrEqualTo(1900).LessThanOrEqualTo(DateTime.UtcNow.Year);
